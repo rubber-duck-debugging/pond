2992cc8882e4b85f45469ca875233dfc3063a944
Max Ribbans <magicmax@maxs-mbp.lan>
2019-06-22 17:49:47 -0400

    Co-authored-by: Max Bender <maxbbender@gmail.com>

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from . import db
-
+from sqlalchemy.orm import relationship
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     firstname = db.Column(db.String(64))
+
+    questions = relationship("Question", backref='user')
 
     def __init__(self, username, firstname):
         self.username = username
@@ -12,14 +15,17 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %s>' % (self.username)
+    
 
 class Question(db.Model):
+    __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text)
     resolved = db.Column(db.Boolean)
-    asker = db.Column(db.Integer)
+    asker = db.Column(db.Integer, db.ForeignKey('users.id'))
     date_asked = db.Column(db.DateTime, nullable=False)
     date_resolved = db.Column(db.DateTime, nullable=True)
+
 
     def __init__(self, text, asker, date_asked, resolved=False, date_resolve=None):
         self.text = text
